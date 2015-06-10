0f4b1a40620b0e5fa75bd679a6d0706701a9340b
John <je.drake@uws.edu.au>
2015-06-10 17:14:05 +1000

    Modest improvements.

--- R/GLAHD-RvsT.R
+++ R/GLAHD-RvsT.R
@@ -4,14 +4,14 @@
 
 
 #- load libraries from script
-source("C:/Repos/GLAHD/R/loadLibraries.R")
+source("R/loadLibraries.R")
 
 
 
 #-----------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------
 #- read in the data, do a few conversions
-dat <- read.csv("C:/Repos/GLAHD/Data/GasEx/RvsT/GLADH RvsT gasex processeed.csv",stringsAsFactors=T)
+dat <- read.csv("Data/GasEx/RvsT/GLADH RvsT gasex processeed.csv",stringsAsFactors=T)
 dat$Date <- as.Date(dat$Date,format="%d/%m/%Y")
 dat$Taxa <- as.factor(sapply(1:nrow(dat),function(i)strsplit(as.character(dat$Code[i]),split="-")[[1]][1])) # get the "Taxa" factor out of Code. Note this could have been a loop
 dat$R_area <- dat$Photo*-1
@@ -23,11 +23,10 @@ rt$R_mass <- rt$R_area/10000*rt$Area/rt$mass*1000 #note mass was in g
 
 
 rt <- subset(rt,Taxa!="SMIT" & Taxa !="ACAM")
-rt$Taxa <- factor(rt$Taxa,levels=c("BTER","BOT","CTER","BRA"))
+rt$Taxa <- factor(rt$Taxa,levels=c("BOT","BTER","BRA","CTER"))
 rt$lnRmass <- log(rt$R_mass)
-rt <- subset(rt,Code!="CTER-4" & Code!="CTER-22") #CTER4 looks weird... peaks much much lower than others. CTER-22 also looks bad
+rt <- subset(rt,Code!="CTER-4" & Code!="CTER-22" & Code!="CTER-27") #CTER4 looks weird... peaks much much lower than others. CTER-22 also looks bad
 rt$Code <- factor(rt$Code)
-
 #-----------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------
 
@@ -48,7 +47,7 @@ rt.m.l <- split(rt.m,rt.m$Taxa)
 windows(20,20);par(mfrow=c(2,2),mar=c(3,2,1,1),oma=c(4,4,1,1))
 for (i in 1:length(rt.m.l)){
   toplot <- subset(rt.m.l[[i]],Tleaf_bin_mid < 40)
-  plotBy(R_mass.mean~Tleaf_bin_mid|Treatment,data=toplot,ylim=c(0,40),pch=15,
+  plotBy(R_mass.mean~Tleaf_bin_mid|Treatment,data=toplot,ylim=c(0,40),pch=15,col=c("blue","red"),legend=ifelse(i==1,T,F),
          panel.first=adderrorbars(x=toplot$Tleaf_bin_mid,y=toplot$R_mass.mean,SE=toplot$R_mass.standard.error,direction="updown",col="black"))
   title(main=toplot$Taxa[i],line=0.2)
   
@@ -182,11 +181,21 @@ params.m <- summaryBy(mt.x + mt.y + mt.Rref~Taxa+Treatment,FUN=c(mean,standard.e
 #-----------------------------------------------------------------------------------------------------
 #-- visualize polynomial model results
 params$R25 <- with(params,exp(poly.a+poly.b*25+poly.c*25^2))
-params$Q10 <- with(params,exp(10*(poly.b+2*poly.c*25)))
+params$R18.5 <- with(params,exp(poly.a+poly.b*18.5+poly.c*18.5^2))
+params$R28.5 <- with(params,exp(poly.a+poly.b*28.5+poly.c*28.5^2))
+params$Q10_25 <- with(params,exp(10*(poly.b+2*poly.c*25)))
+params$Q10_18.5 <- with(params,exp(10*(poly.b+2*poly.c*18.5)))
+params$Q10_28.5 <- with(params,exp(10*(poly.b+2*poly.c*28.55)))
 # 
 windows(20,30);par(mar=c(7,6,1,1),mfrow=c(2,1))
 boxplot(R25~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="R25")
-boxplot(Q10~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="Q10 at 25 C")
+boxplot(Q10_25~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="Q10 at 25 C")
+
+boxplot(R18.5~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="R18.5")
+boxplot(Q10_18.5~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="Q10 at 18.5 C")
+
+boxplot(R28.5~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="R28.5")
+boxplot(Q10_28.5~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="Q10 at 28.5 C")
 # 
 # lmR <- lm(R25~Treatment*Taxa,data=params)
 # lmQ10 <- lm(Q10~Treatment*Taxa,data=params)
@@ -199,29 +208,95 @@ boxplot(Q10~Treatment*Taxa,data=params,las=2,col=c("blue","red"),ylab="Q10 at 25
 
 
 
+
+#-----------------------------------------------------------------------------------------------------
+#- predict R and Q10 across all temperatures for all plants
+tvals <- seq(from=15,to=40,by=1)
+params.list <- split(params,params$Code)
+predR <- list()
+for(i in 1:length(params.list)){
+  predR[[i]] <- data.frame("Code"=params.list[[i]]$Code[1],"Treatment"=params.list[[i]]$Treatment[1],
+                            "Taxa"=params.list[[i]]$Taxa[1],"T"=tvals,
+                           "R"=with(params.list[[i]],exp(poly.a+poly.b*tvals+poly.c*tvals^2)),
+                           "Q10"=with(params.list[[i]],exp(10*(poly.b+2*poly.c*tvals))))
+}
+predR.df <- do.call(rbind,predR)
+predR.df$logR <- log(predR.df$R)
+plotBy(R~T|Treatment,data=predR.df)
+plotBy(Q10~T|Treatment,data=predR.df)
+
+
+#-------------------------------------------------------------------------------------------------------------------------
+#- plot R with confidence intervals for all taxa overlapping
+predR.df$Taxa_Treat <- as.factor(paste(predR.df$Taxa,predR.df$Treatment,sep="_"))
+dat.l <- split(predR.df,predR.df$Taxa_Treat)
+
+windows(18,25);par(mfrow=c(2,1),mar=c(4,5,1,1))
+fit.sp <- list()
+newdat <- list()
+plot(Q10~T,data=dat.l[[1]],type="p",col="white",axes=F,xlab="",ylab="",xpd=NA,ylim=c(1,3.5))
+colors <- c("grey","grey","green","green","yellow","yellow","cyan","cyan")
+fills <- rep(c(0.3,0.9),4)
+for (i in 1:length(dat.l)){
+  dat.temp <- dat.l[[i]]
+  fit.sp[[i]] <-  lm(Q10~T,data=dat.l[[i]])
+
+  newdat[[i]] <- expand.grid(T=seq(from=15,to=40,length.out=99),lower=NA,upper=NA)
+  newdat[[i]]$wpred <- predict(fit.sp[[i]],newdat[[i]],se.fit=T)$fit
+  newdat[[i]]$se <- predict(fit.sp[[i]],newdat[[i]],se.fit=T)$se.fit
+  newdat[[i]]$upper <- with(newdat[[i]],wpred+se)
+  newdat[[i]]$lower <- with(newdat[[i]],wpred-se)
+  
+#   b <- bootCase(fit.sp[[i]])
+#   for(j in 1:nrow(newdat[[i]])){
+#     b02 <- SSasymp(newdat[[i]]$TDR[j],Asym=b[,"Asym"],lrc=b[,"lrc"],R0=b[,"R0"])
+#     newdat[[i]]$lower[j] <- unname(quantile(b02,probs=c(0.025,0.975)))[1]
+#     newdat[[i]]$upper[j] <- unname(quantile(b02,probs=c(0.025,0.975)))[2]
+#     
+#   }
+  lines(wpred~T,data=newdat[[i]])
+  polygon(x = c(newdat[[i]]$T, rev(newdat[[i]]$T)), y = c(newdat[[i]]$lower, rev(newdat[[i]]$upper)), col = alpha(colors[i],fills[i]), border = NULL,xpd=F)
+}
+magaxis(side=1:4,labels=c(1,1,0,0),las=1)
+title(ylab=expression(Q[10]),xlab=expression(Leaf~Temperature~(degree*C)),cex.lab=1.5,las=1)
+
+#-----------------------------------------------------------------------------------------------------
+
+
+
 #-----------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------
 #- statistically analyze R25 and Q10 estimates
 library(effects)
 
 #- R25
+lm.R18.5 <- lm(R18.5~Taxa*Treatment,data=params)
 lm.R25 <- lm(R25~Taxa*Treatment,data=params)
+lm.R28.5 <- lm(R28.5~Taxa*Treatment,data=params)
 
 #look at model diagnostics
 plot(lm.R25)
 hist(lm.R25$residuals)
+anova(lm.R18.5)
 anova(lm.R25)
+anova(lm.R28.5)
 plot(allEffects(lm.R25))     
 
-
+effect("Treatment",lm.R25)
+(11-9.636)/11
 
 #- Q10
-lm.Q10 <- lm(Q10~Taxa*Treatment,data=params)
+lm.Q10_18.5 <- lm(Q10_18.5~Taxa*Treatment,data=params)
+lm.Q10_25 <- lm(Q10_25~Taxa*Treatment,data=params)
+lm.Q10_28.5 <- lm(Q10_28.5~Taxa*Treatment,data=params)
 
 #look at model diagnostics
 plot(lm.Q10) # point 37 may need to be removed
 hist(lm.Q10$residuals)
-anova(lm.Q10)
-plot(allEffects(lm.Q10))     
+anova(lm.Q10_18.5)
+anova(lm.Q10_25)
+anova(lm.Q10_28.5)
+plot(effect("Taxa:Treatment",lm.Q10_25))
+plot(allEffects(lm.Q10)) 
 #-----------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------
