526405dda91a37392163c5c4a753bfdd1d4a20ac
AngeVar <angelica.varhammar@gmail.com>
2015-07-10 15:25:40 +1000

    analysis of gam function - seems to be fitting the data very well

--- R/GLAHD-growth_analysis_GAM.R
+++ R/GLAHD-growth_analysis_GAM.R
@@ -85,6 +85,7 @@ gamfits2 <- merge(gamfits.df,key,by=c("Code"),all.x=T)
 #-------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------
 
+gamfits2$AGR <- gamfits2$dydt*gamfits2$predMass
 
 
 
@@ -92,12 +93,16 @@ gamfits2 <- merge(gamfits.df,key,by=c("Code"),all.x=T)
 #-------------------------------------------------------------------------------------
 #- plot some results
 
-g.trt <- summaryBy(dydt~Time+Treatment+Location+Range,data=gamfits2,FUN=mean,keep.names=T)
+g.trt <- summaryBy(dydt+predMass+AGR~Time+Treatment+Location+Range,data=gamfits2,FUN=mean,keep.names=T)
 g.trt$combotrt <- as.factor(paste(g.trt$Location,g.trt$Range,g.trt$Treatment,sep="_"))
 
 windows(40,30);par(mfrow=c(1,1))
 plotBy(dydt~Time|combotrt,data=g.trt,col=c("red","black","blue","green","orange","cyan","grey","yellow"),
        legendwhere="topright",pch=15)
+plotBy(AGR~Time|combotrt,data=g.trt,col=c("red","black","blue","green","orange","cyan","grey","yellow"),
+       legendwhere="topleft",pch=15)
+plotBy(predMass~Time|combotrt,data=g.trt,col=c("red","black","blue","green","orange","cyan","grey","yellow"),
+       legendwhere="topleft",pch=15)
 
 #maximum of RGR
 maxdydt<-gamfits2[ gamfits2$dydt %in% tapply(gamfits2$dydt, gamfits2$Code, max), ]#max RGR per Code
@@ -141,6 +146,7 @@ plot(allEffects(fm.tmaxdydt))
 #-------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------
+
 #- Calculate interval-based RGR for each plant
 dat3 <- dat3[with(dat3,order(Code,Date)),] #- make sure the observations for each plant are in order
 growth.l <- split(dat3,dat3$Code)          #- split into a list for each plant
@@ -176,7 +182,163 @@ for (i in 1:length(dat4.l)){
 mtext(side=1,"Time (days after treatment began)",outer=T,line=4,cex=1.5,xpd=NA)
 mtext(side=2,"RGR",outer=T,line=3,cex=1.5)
 
+#-------------------------------------------------------------------------------------
+#- plot mass and RGR over time for each taxa-treatment combination, overlay data and model output
+dat4$RGR_time <- dat4$Time+5
+dat4.l.taxa <- split(dat4,dat4$Taxa)
+rates.taxa <- summaryBy(predMass+dydt+AGR~Time+Treatment+Location+Range+Taxa,data=gamfits2,FUN=mean,keep.names=T)
+rates.taxa.l <- split(rates.taxa,rates.taxa$Taxa)
+rates.l <- split(gamfits2,gamfits2$Taxa)
+
+# loop over each taxa, plot a set of four figures, export to a pdf
+pdf(file="C:/Repos/GLAHD/Output/RGR_M_Taxa_filtered_GAM.pdf")
+
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE), 
+       widths=rep(1,4), heights=rep(1,4))
+for (i in 1:length(dat4.l.taxa)){
+  ylims=c(0,max(dat4.l.taxa[[i]]$TotMass+5))
+  #- plot home mass over time
+  plot(TotMass~Time,data=subset(dat4.l.taxa[[i]],Treatment=="Home"),ylim=ylims,col="blue",
+       main=paste(subset(dat4.l.taxa[[i]],Treatment=="Home")$Taxa[1],"Home",sep='_'))
+  plotBy(predMass~Time|Code,data=subset(rates.l[[i]],Treatment=="Home"),lwd=0.5,add=T,type="l",legend=F,col="grey")
+  lines(predMass~Time,data=subset(rates.taxa.l[[i]],Treatment=="Home"),lwd=3)
+  
+  #- plot warmed mass over time
+  plot(TotMass~Time,data=subset(dat4.l.taxa[[i]],Treatment=="Warmed"),ylim=ylims,col="red",
+       main=paste(subset(dat4.l.taxa[[i]],Treatment=="Warmed")$Taxa[1],"Warmed",sep='_'))
+  plotBy(predMass~Time|Code,data=subset(rates.l[[i]],Treatment=="Warmed"),lwd=0.5,add=T,type="l",legend=F,col="grey")
+  lines(predMass~Time,data=subset(rates.taxa.l[[i]],Treatment=="Warmed"),lwd=3)
+  
+  #- plot home RGR over time
+  ylims=c(0,max(dat4.l.taxa[[i]]$RGR+0.1,na.rm=T))
+  
+  plot(RGR~RGR_time,data=subset(dat4.l.taxa[[i]],Treatment=="Home"),ylim=ylims,col="blue",xlim=c(0,60),
+       main=paste(subset(dat4.l.taxa[[i]],Treatment=="Home")$Taxa[1],"Home",sep='_'))
+  plotBy(dydt~Time|Code,data=subset(rates.l[[i]],Treatment=="Home"),lwd=0.5,add=T,type="l",legend=F,col="grey")
+  lines(dydt~Time,data=subset(rates.taxa.l[[i]],Treatment=="Home"),lwd=3)
+  
+  #- plot warmed RGR over time
+  plot(RGR~RGR_time,data=subset(dat4.l.taxa[[i]],Treatment=="Warmed"),ylim=ylims,col="red",xlim=c(0,60),
+       main=paste(subset(dat4.l.taxa[[i]],Treatment=="Warmed")$Taxa[1],"Home",sep='_'))
+  plotBy(dydt~Time|Code,data=subset(rates.l[[i]],Treatment=="Warmed"),lwd=0.5,add=T,type="l",legend=F,col="grey")
+  lines(dydt~Time,data=subset(rates.taxa.l[[i]],Treatment=="Warmed"),lwd=3)
+  
+}
+dev.off()
+
+#-------------------------------------------------------------------------------------------------------
+#- Do analysis suggested by Mark, where interval-based RGR estimates are compared to model estimates
+#-  The basic question: is a 4th order polynomial model sufficient to describe the data,
+#-      particularly the peaked RGR response to warming during the second growth interval?
+
+
+#- the interval-based RGR estimate for the segment in question
+rgr.seg2 <- subset(dat4,Date==as.Date("2014-11-17"))
+names(rgr.seg2)[18] <- "RGR.seg"
+
+#- get the modeled RGR for each day of the growth interval in question for each plant
+#remember that time "1" is 2014-11-07
+table(dat2$Date) # second growth interval is from 2014-11-17 to 2014-11-26, which is "Time" 11 to 20
+rgr.mod.seg2 <- subset(gamfits2,Time>=11 & Time <=20)
+rgr.mod.seg2.plant <- summaryBy(predMass+AGR+dydt~Code+Species+Treatment+Location+Taxa+Range,data=rgr.mod.seg2,keep.names=T)
+
+#- put them together
+rgr2 <- merge(rgr.seg2,rgr.mod.seg2.plant,by=c("Code","Species","Treatment","Location","Taxa","Range"))
+
+#- plot both estimates of RGR for each taxa
+
+#plot RGR seg, then RGR modeled
+ylims=c(0,0.2)
+xlims=c(0,35)
+colors <- c("blue","red")
+windows(10,10);par(mfrow=c(2,1),mar=c(0.5,6,0.5,3),oma=c(6,0,0,0),cex.axis=1.2)
+
+#RGR segmented
+boxplot(RGR.seg~Treatment+Taxa,data=rgr2,ylim=ylims,xlim=xlims,
+        axes=F,las=2,col=colors)
+magaxis(c(2,3,4),labels=c(1,0,1),box=T,las=1)
+title(ylab=expression(RGR[seg]),cex.lab=2,line=2.5)
+axis(side=1,at=seq(from=1.5,to=34.5,by=2),labels=F,las=2,cex.axis=1.5)
+abline(v=16.4)
+
+#RGR modeled
+boxplot(dydt~Treatment+Taxa,data=rgr2,ylim=ylims,xlim=xlims,
+        axes=F,las=2,col=colors)
+magaxis(c(2,3,4),labels=c(1,0,1),box=T,las=1)
+title(ylab=expression(RGR[mod]),cex.lab=2,line=2.5)
+axis(side=1,at=seq(from=1.5,to=34.5,by=2),labels=levels(rgr2$Taxa),las=2,cex.axis=1.5)
+abline(v=16.4)
+
+
+#- by-plot
+windows(20,20)
+plotBy(dydt~RGR.seg|Location,data=rgr2,xlab="RGR-seg",ylab="RGR-mod",xlim=c(0,0.22),ylim=c(0,0.22),cex.lab=2)
+abline(0,1)
+
+#- compare statistical analyes
+library(nlme)
+rgr2$Sp_RS_EN <- as.factor(with(rgr2,paste(Species,Range)))   # use "explicit nesting" to create error terms of species:rangesize and prov:species:rangesize
+rgr2$Prov_Sp_EN <- as.factor(with(rgr2,paste(Taxa,Species)))
+fm.rgr.mod <- lme(log(dydt)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=rgr2)
+plot(fm.rgr.mod,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
+plot(fm.rgr.mod,log(dydt)~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
+plot(fm.rgr.mod,log(dydt)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
+qqnorm(fm.rgr.mod, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
+hist(fm.rgr.mod$residuals[,1])
+anova(fm.rgr.mod)  
+
+fm.rgr.seg <- lme(RGR.seg~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=rgr2)
+plot(fm.rgr.seg,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
+plot(fm.rgr.seg,RGR.seg~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
+plot(fm.rgr.seg,RGR.seg~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
+qqnorm(fm.rgr.seg, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
+hist(fm.rgr.seg$residuals[,1])
+anova(fm.rgr.seg)  
+
+
 #-----------------------------------------------------------------------------------------
+#Relative enhancement of biomass
+ber<- summaryBy(predMass~Time+Range+Location+Treatment, data=gamfits2, FUN=mean, keep.names=T) 
+berh <- subset(ber, Treatment == "Home")
+berw <- subset (ber, Treatment == "Warmed")  
+bio <- cbind(berh,berw)
+bio$ber<- bio[,10]/bio[,5]
+BER<- bio[,c(1:5,11)]
+bersum<- summaryBy(ber~Range+Location+Time, data=BER, FUN=mean, keep.names=T)
+
+windows(10,6);par(mfrow=c(1,2),mar=c(2,0,2,0),oma=c(5,9,3,5),cex.axis=1)
+
+plotBy(ber~Time|Range,data=subset(BER, Location =="S"),col=c("black","red","blue","orange"),
+       legend=F,type="o", main="South", ylim=c(0.5,2), xlim=c(0,67))
+mtext(text=expression(M[W]:M[H]),side=2,outer=T,cex=1,adj=0.5,line=3)
+abline(h=1)
+plotBy(ber~Time|Range,data=subset(BER,Location =="N"),col=c("black","red","blue","orange"),
+       legendwhere="topright",type="o", main = "North", ylim=c(0.5,2),yaxt='n', xlim=c(0,67))
+mtext(text="Time",side=1,outer=T,cex=1,adj=0.5,line=1)
+mtext(text="Relative Enhancement of M",side=3,outer=T,cex=1,adj=0.5,line=1)
+abline(h=1)
+
+#Absolute enhancement of biomass
+ber<- summaryBy(predMass~Time+Range+Location+Treatment, data=gamfits2, FUN=mean, keep.names=T) 
+berh <- subset(ber, Treatment == "Home")
+berw <- subset (ber, Treatment == "Warmed")  
+bio <- cbind(berh,berw)
+bio$ber<- bio[,10]-bio[,5]
+BER<- bio[,c(1:5,11)]
+bersum<- summaryBy(ber~Range+Location+Time, data=BER, FUN=mean, keep.names=T)
+
+windows(10,6);par(mfrow=c(1,2),mar=c(2,0,2,0),oma=c(5,9,3,5),cex.axis=1)
+
+plotBy(ber~Time|Range,data=subset(BER, Location =="S"),col=c("black","red","blue","orange"),
+       legend=F,type="o", main="South", ylim=c(-5,20), xlim=c(0,67))
+mtext(text=expression(M[W]-M[H]),side=2,outer=T,cex=1,adj=0.5,line=3)
+abline(h=0)
+plotBy(ber~Time|Range,data=subset(BER,Location =="N"),col=c("black","red","blue","orange"),
+       legendwhere="topright",type="o", main = "North", ylim=c(-5,20),yaxt='n', xlim=c(0,67))
+mtext(text="Time",side=1,outer=T,cex=1,adj=0.5,line=1)
+mtext(text="Absolute Enhancement of M",side=3,outer=T,cex=1,adj=0.5,line=1)
+abline(h=0)
+
 #Relative enhancement of RGR
 ber<- summaryBy(dydt~Time+Range+Location+Treatment, data=gamfits2, FUN=mean, keep.names=T) 
 berh <- subset(ber, Treatment == "Home")
@@ -189,11 +351,11 @@ bersum<- summaryBy(ber~Range+Location+Time, data=BER, FUN=mean, keep.names=T)
 windows(10,6);par(mfrow=c(1,2),mar=c(2,0,2,0),oma=c(5,9,3,5),cex.axis=1)
 
 plotBy(ber~Time|Range,data=subset(BER, Location =="S"),col=c("black","red","blue","orange"),
-       legend=F,type="o", main="South", ylim=c(1,1.5), xlim=c(0,67))
+       legend=F,type="o", main="South", ylim=c(0.5,1.5), xlim=c(0,67))
 mtext(text=expression(RGR[W]:RGR[H]),side=2,outer=T,cex=1,adj=0.5,line=3)
 abline(h=1)
 plotBy(ber~Time|Range,data=subset(BER,Location =="N"),col=c("black","red","blue","orange"),
-       legendwhere="topright",type="o", main = "North", ylim=c(1,1.5),yaxt='n', xlim=c(0,67))
+       legendwhere="topright",type="o", main = "North", ylim=c(0.5,1.5),yaxt='n', xlim=c(0,67))
 mtext(text="Time",side=1,outer=T,cex=1,adj=0.5,line=1)
 mtext(text="Relative Enhancement of RGR",side=3,outer=T,cex=1,adj=0.5,line=1)
 abline(h=1)
@@ -226,5 +388,17 @@ mtext(text="Absolute Enhancement of RGR",side=3,outer=T,cex=1,adj=0.5,line=1)
 abline(h=0)
 
 #--------------------------------------------------------------------------------------------------
+#LAR
+
+rate <- merge(gamfits2,dat2,by=c("Code","Time","Species","Location","Treatment","Taxa","Range"))[,c(1:10,19)]# merge with dat2 to get LA
+rate$LAR <- rate$leafArea/rate$predMass
+rate.trt <- summaryBy(LAR+predMass+dydt+AGR~Time+Treatment+Location+Range,data=rate,FUN=mean,keep.names=T)
+rate.trt$combotrt <- as.factor(paste(rate.trt$Location,rate.trt$Range,rate.trt$Treatment,sep="_"))
+
+windows(40,30);par(mfrow=c(1,1))
+plotBy(LAR~Time|combotrt,type='o',data=rate.trt,col=c("red","black","blue","green","orange","cyan","grey","yellow"),
+       legendwhere="topright",pch=15)
+plotBy(LAR~dydt|combotrt, type='o', data=rate.trt,col=c("red","black","blue","green","orange","cyan","grey","yellow"),
+       legendwhere="bottomright",pch=15, ylim=c(60,144), xlab="RGR")
 
 
