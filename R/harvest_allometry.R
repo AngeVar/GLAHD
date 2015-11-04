3ce608a5e30016993e94a79f008d4f6b366a70d0
AngeVar <angelica.varhammar@gmail.com>
2015-11-04 11:52:22 +1100

    fixed scale SLA, mass already in grams

--- R/harvest_allometry.R
+++ R/harvest_allometry.R
@@ -13,7 +13,7 @@ dat <- read.csv("Data/Harvests/GHS39_GLAHD_MAIN_BIOMASS_20141106-20150116_L1.csv
 dat$Date <- as.Date(dat$Date,format="%d/%m/%Y")
 dat$Totmass <- base::rowSums(dat[,11:13]) #total mass is the sum of leaf, stem, and root mass
 dat$LAR <- with(dat,Leafarea/Totmass)
-dat$SLA <- with(dat,Leafarea/Leafmass)
+dat$SLA <- with(dat,(Leafarea/10000)/(Leafmass))#m2/g
 dat$d2h <- with(dat,(Diameter/10)^2*(Height)) #calculate d2h in cm3
 dat$logd2h <- log10(dat$d2h)
 dat$logTM <- log10(dat$Totmass)
@@ -394,40 +394,60 @@ dev.copy2pdf(file="W:/WorkingData/GHS39/GLAHD/Share/Output/LAR_TotalMass.pdf")
 
 
 
+#plot sla
+dat$Range<- as.factor(ifelse(dat$Species == "TER"|dat$Species == "CAM", "wide","narrow"))
+dat2<-droplevels(subset(dat,Treat!="Pre"))
+dat2$combotrt <- as.factor(paste(dat2$Location,dat2$Range,dat2$Treatment,sep="_"))
 
+windows(20,15);par(mfrow=c(1,2),mar=c(2,0,1,0),oma=c(5,9,3,5),cex.axis=1.2)
+
+ylims=c(0,0.04)
+boxplot(SLA~Treatment*Range,data=subset(dat2,Location=="N"),ylim=ylims,
+        axes=F,las=2,col=c("red","blue"))
+legend("topleft","Tropical",bty="n",cex=1.5,inset=-0.05)
+magaxis(c(2,3,4),labels=c(1,0,0),frame.plot=T,las=1)
+mtext(text=expression(SLA),side=2,outer=T,cex=2,line=5)
+mtext(text=expression("("*m^2~g^-1*")"),side=2,outer=T,cex=1,line=3)
+axis(side=1,at=c(1.5,3.5),labels=levels(dat2$Range),las=1,cex.axis=1.5)
+boxplot(SLA~Treatment*Range,data=subset(dat2,Location=="S"),ylim=ylims,
+        axes=F,las=2,col=c("red","blue"))
+legend("topleft","Temperate",bty="n",cex=1.5,inset=-0.05)
+magaxis(c(2,3,4),labels=c(0,0,1),frame.plot=T,las=1)
+axis(side=1,at=c(1.5,3.5),labels=levels(dat2$Range),las=1,cex.axis=1.5)
+mtext(text=expression(Range~size),side=1,outer=T,cex=2,line=3)
 
 
 #-----------------------------------
 #- plot SLA vs. totalmass allometries for all taxa
-combos <- levels(dat$Taxa)
+combos <- levels(dat2$Taxa)
 #names(combos) <- c("Treat","Taxa")
 windows(20,20)
 par(mfrow=c(5,4),mar=c(0,0,0,0),oma=c(6,6,1,1))
-for (i in 1:length(levels(dat$Taxa))){
+for (i in 1:length(levels(dat2$Taxa))){
   dat2 <- subset(dat,Taxa==as.character(combos[i]))
   
-  with(subset(dat2,Treat=="Home"),plot(SLA~logTM,xlim=c(-1.3,2.2),ylim=c(80,500),col=Treat,pch=15,axes=F,cex=1.5,
+  with(subset(dat2,Treat=="Home"),plot(SLA~logTM,xlim=c(-1.3,2.2),ylim=c(4,40),col="green",pch=15,axes=F,cex=1.5,
                                          xlab=expression(log[10]~(Total~mass~(g))),
                                          ylab=expression(log[10]~Leaf~mass~(g))))  
   fit <- lm(SLA~logTM,data=subset(dat2,Treat=="Home"))
-  predline(fit,col=alpha("red",alpha=0.1))
-  
-  
-  with(subset(dat2,Treat=="Warmed"),points(SLA~logTM,xlim=c(-1.3,2.2),ylim=c(-2.2,1),col=Treat,pch=15,cex=1.5,
-                                         xlab=expression(log[10]~(Total~mass~(g))),
-                                         ylab=expression(log[10]~Leaf~mass~(g))))  
-  fit <- lm(SLA~logTM,data=subset(dat2,Treat=="Warmed"))
   predline(fit,col=alpha("green",alpha=0.1))
   
   
-  with(subset(dat2,Treat=="Pre"),points(SLA~logTM,xlim=c(-1.3,2.2),ylim=c(-2.2,1),col=Treat,pch=15,cex=1.5,
+  with(subset(dat2,Treat=="Warmed"),points(SLA~logTM,xlim=c(-1.3,2.2),ylim=c(4,40),col="red",pch=15,cex=1.5,
+                                         xlab=expression(log[10]~(Total~mass~(g))),
+                                         ylab=expression(log[10]~Leaf~mass~(g))))  
+  fit <- lm(SLA~logTM,data=subset(dat2,Treat=="Warmed"))
+  predline(fit,col=alpha("red",alpha=0.1))
+  
+  
+  with(subset(dat2,Treat=="Pre"),points(SLA~logTM,xlim=c(-1.3,2.2),ylim=c(4,40),col="black",pch=15,cex=1.5,
                                            xlab=expression(log[10]~(Total~mass~(g))),
                                            ylab=expression(log[10]~Leaf~mass~(g))))  
   magaxis(side=1:4,labels=c(1,1,0,0),box=T)
   legend("bottom",legend=dat2$Taxa[1],bty="n",cex=1.5)
 }
 mtext(expression(log[10]~(Total~mass~(g))),side=1,line=2,outer=T,cex=1.5)
-mtext(expression(Specific~leaf~area~(cm^2*g^-1)),side=2,line=2,outer=T,cex=1.5)
+mtext(expression(Specific~leaf~area~(m^2*g^-1)),side=2,line=2,outer=T,cex=1.5)
 legend(x=4,y=380,legend=c("Warmed","Pre","Home"),pch=15,cex=1.5,xpd=NA,col=colors[1:3])
 dev.copy2pdf(file="W:/WorkingData/GHS39/GLAHD/Share/Output/SLA_TotalMass.pdf")
 #-----------------------------------
@@ -565,9 +585,7 @@ plot(allEffects(fm1LA))
 plot(effect("Totmass:Range",fm1LA), multiline=TRUE) #- compares slopes (overlayed)
 plot(effect("Totmass:Location",fm1LA), multiline=TRUE) #- compares slopes (overlayed)
 plot(effect("Totmass:Treatment",fm1LA), multiline=TRUE) #- compares slopes (overlayed)
-plot(effect("Totmass:Treatment:Location",fm1LA), multiline=TRUE) #- compares slopes (overlayed)
 
-coef(summary(fm1LA))
 
 
 
