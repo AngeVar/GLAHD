f5326258b336d791dc6be377eb313df971dc6955
Angelica Varhammar <30034792@westernsydney.edu.au>
2017-02-08 10:30:55 +1100

    made figures of area-based and mass-based rates per provenance in response to warming. Studies the effect of removing SMIT from mixed models.

--- R_cleaned/Figure 6.R
+++ R_cleaned/Figure 6.R
@@ -6,8 +6,8 @@ acifits$JtoVm<-with(acifits,Jmaxm/Vcmaxm)
 
 asat.tm <- summaryBy(Photo~Taxa+Treatment+Location+Range,data=Asat,FUN=mean,keep.names=T)
 Rmass.tm <- summaryBy(Rmass~Taxa+Treatment+Location+Range,data=Rdark,FUN=mean,keep.names=T)
-jmax.tm <- summaryBy(Jmax~Taxa+Treatment+Location+Range,data=acifit,FUN=mean,keep.names=T)
-vcmax.tm <- summaryBy(Vcmax~Taxa+Treatment+Location+Range,data=acifit,FUN=mean,keep.names=T)
+jmax.tm <- summaryBy(Jmax~Taxa+Treatment+Location+Range,data=acifits,FUN=mean,keep.names=T)
+vcmax.tm <- summaryBy(Vcmax~Taxa+Treatment+Location+Range,data=acifits,FUN=mean,keep.names=T)
 
 colors <- c(alpha("black",0.4),"red")
 #with SMIT
@@ -62,62 +62,62 @@ magaxis(c(2,4),labels=c(0,0),frame.plot=T,las=1,cex.axis=1.2)
 axis(side=1,at=c(1.5,3.5),labels=c("Narrow","Wide"),las=1,cex.axis=1.5)
 legend("topright","f", bty="n", cex=1.5)
 
-#W/o SMIT
-jmax.tms<- subset(jmax.tm, Taxa != "SMIT")
-vcmax.tms<- subset(vcmax.tm, Taxa != "SMIT")
-asat.tms<- subset(asat.tm, Taxa != "SMIT")
-
-windows(11.69,11.69);par(mfrow=c(3,2),mar=c(0,0,0,0),oma=c(6,9,6,9))
-#Jmax
-ylims=c(90,250)
-boxplot(Jmax~Treatment*Range,data=subset(jmax.tms,Location=="S"),ylim=ylims,
-        axes=F,las=2,col=colors)
-magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
-legend("topleft", legend=c(expression(Warmed~(+3.5~degree~C)),"Home"),pch=22, pt.cex=2, pt.bg=c(alpha("red",0.6),alpha("black",0.6)),
-       bty="n",cex=1.2)
-
-mtext(text=expression(J["max"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
-mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=F,cex=1,adj=0.5,line=3)
-mtext(text="Temperate", side=3, line=0.5, cex=1.2)
-legend("topright","a", bty="n", cex=1.5)
-
-boxplot(Jmax~Treatment*Range,data=subset(jmax.tms,Location=="N"),ylim=ylims,
-        axes=F,las=2,col=colors)
-magaxis(c(2,4),labels=c(0,0),frame.plot=T,las=1,cex.axis=1.2)
-mtext(text="Tropical", side=3, line=0.5, cex=1.2)
-legend("topright","b", bty="n", cex=1.5)
-
-
-#Vcmax
-ylims=c(60,220)
-boxplot(Vcmax~Treatment*Range,data=subset(vcmax.tms,Location=="S"),ylim=ylims,
-        axes=F,las=2,col=colors)
-magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
-mtext(text=expression(V["cmax"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
-mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=T,cex=1,adj=0.5,line=3)
-legend("topright","c", bty="n", cex=1.5)
-
-boxplot(Vcmax~Treatment*Range,data=subset(vcmax.tms,Location=="N"),ylim=ylims,
-        axes=F,las=2,col=colors)
-magaxis(c(2,4),labels=c(0,0),frame.plot=T,las=1,cex.axis=1.2)
-legend("topright","d", bty="n", cex=1.5)
-
-#Asat
-ylims=c(20,35)
-boxplot(Photo~Treatment*Range,data=subset(asat.tms,Location=="S"),ylim=ylims,
-        axes=F,las=2,col=colors)
-magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
-mtext(text=expression(A["sat"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
-mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=T,cex=1,adj=0.12,line=3)
-axis(side=1,at=c(1.5,3.5),labels=c("Narrow","Wide"),las=1,cex.axis=1.5)
-legend("topright","e", bty="n", cex=1.5)
-
-boxplot(Photo~Treatment*Range,data=subset(asat.tms,Location=="N"),ylim=ylims,
-        axes=F,las=2,col=colors)
-magaxis(c(2,4),labels=c(0,0),frame.plot=T,las=1,cex.axis=1.2)
-axis(side=1,at=c(1.5,3.5),labels=c("Narrow","Wide"),las=1,cex.axis=1.5)
-legend("topright","f", bty="n", cex=1.5)
-##---------------------------------------------------------------------------------------------
+# #W/o SMIT
+# jmax.tms<- subset(jmax.tm, Taxa != "SMIT")
+# vcmax.tms<- subset(vcmax.tm, Taxa != "SMIT")
+# asat.tms<- subset(asat.tm, Taxa != "SMIT")
+# 
+# windows(11.69,11.69);par(mfrow=c(3,2),mar=c(0,0,0,0),oma=c(6,9,6,9))
+# #Jmax
+# ylims=c(90,250)
+# boxplot(Jmax~Treatment*Range,data=subset(jmax.tms,Location=="S"),ylim=ylims,
+#         axes=F,las=2,col=colors)
+# magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+# legend("topleft", legend=c(expression(Warmed~(+3.5~degree~C)),"Home"),pch=22, pt.cex=2, pt.bg=c(alpha("red",0.6),alpha("black",0.6)),
+#        bty="n",cex=1.2)
+# 
+# mtext(text=expression(J["max"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+# mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=F,cex=1,adj=0.5,line=3)
+# mtext(text="Temperate", side=3, line=0.5, cex=1.2)
+# legend("topright","a", bty="n", cex=1.5)
+# 
+# boxplot(Jmax~Treatment*Range,data=subset(jmax.tms,Location=="N"),ylim=ylims,
+#         axes=F,las=2,col=colors)
+# magaxis(c(2,4),labels=c(0,0),frame.plot=T,las=1,cex.axis=1.2)
+# mtext(text="Tropical", side=3, line=0.5, cex=1.2)
+# legend("topright","b", bty="n", cex=1.5)
+# 
+# 
+# #Vcmax
+# ylims=c(60,220)
+# boxplot(Vcmax~Treatment*Range,data=subset(vcmax.tms,Location=="S"),ylim=ylims,
+#         axes=F,las=2,col=colors)
+# magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+# mtext(text=expression(V["cmax"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+# mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=T,cex=1,adj=0.5,line=3)
+# legend("topright","c", bty="n", cex=1.5)
+# 
+# boxplot(Vcmax~Treatment*Range,data=subset(vcmax.tms,Location=="N"),ylim=ylims,
+#         axes=F,las=2,col=colors)
+# magaxis(c(2,4),labels=c(0,0),frame.plot=T,las=1,cex.axis=1.2)
+# legend("topright","d", bty="n", cex=1.5)
+# 
+# #Asat
+# ylims=c(20,35)
+# boxplot(Photo~Treatment*Range,data=subset(asat.tms,Location=="S"),ylim=ylims,
+#         axes=F,las=2,col=colors)
+# magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+# mtext(text=expression(A["sat"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+# mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=T,cex=1,adj=0.12,line=3)
+# axis(side=1,at=c(1.5,3.5),labels=c("Narrow","Wide"),las=1,cex.axis=1.5)
+# legend("topright","e", bty="n", cex=1.5)
+# 
+# boxplot(Photo~Treatment*Range,data=subset(asat.tms,Location=="N"),ylim=ylims,
+#         axes=F,las=2,col=colors)
+# magaxis(c(2,4),labels=c(0,0),frame.plot=T,las=1,cex.axis=1.2)
+# axis(side=1,at=c(1.5,3.5),labels=c("Narrow","Wide"),las=1,cex.axis=1.5)
+# legend("topright","f", bty="n", cex=1.5)
+# ##---------------------------------------------------------------------------------------------
 
 ##---------------------------------------------------------------------------------------------
 
@@ -205,3 +205,105 @@ magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
 mtext(text=expression("From"~A["sat"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
 mtext(text=expression("SLA ("*cm^2~g^-1*")"),side=2,outer=F,cex=1,line=3)
 axis(side=1,at=c(1:34),labels=rep(levels(asatm$Taxa),2),las=1,cex.axis=1)
+
+#taxa specific responses -- Problem: the boxplot is plotting boxes in alphabetical order
+#area based
+
+colors <- c(alpha("black",0.4),"red")
+acifits$TaxTreat<- as.factor(paste(acifits$Taxa,acifits$Treatment, sep="."))
+Asat$TaxTreat<- as.factor(paste(Asat$Taxa,Asat$Treatment, sep="."))
+
+acifits$TaxTreat<- factor(acifits$TaxTreat, levels=c(
+  "ATER.Home","ATER.Warmed","BTER.Home","BTER.Warmed","ACAM.Home","ACAM.Warmed","BCAM.Home","BCAM.Warmed",
+  "CCAM.Home","CCAM.Warmed","BOT.Home","BOT.Warmed","LONG.Home","LONG.Warmed","SMIT.Home","SMIT.Warmed",
+  "CTER.Home","CTER.Warmed","DTER.Home","DTER.Warmed", "ETER.Home", "ETER.Warmed","DCAM.Home","DCAM.Warmed", 
+  "ECAM.Home","ECAM.Warmed","FCAM.Home","FCAM.Warmed","BRA.Home","BRA.Warmed","PEL.Home","PEL.Warmed",
+  "PLAT.Home","PLAT.Warmed"))
+Asat$TaxTreat<- factor(Asat$TaxTreat, levels=c(
+  "ATER.Home","ATER.Warmed","BTER.Home","BTER.Warmed","ACAM.Home","ACAM.Warmed","BCAM.Home","BCAM.Warmed",
+  "CCAM.Home","CCAM.Warmed","BOT.Home","BOT.Warmed","LONG.Home","LONG.Warmed","SMIT.Home","SMIT.Warmed",
+  "CTER.Home","CTER.Warmed","DTER.Home","DTER.Warmed", "ETER.Home", "ETER.Warmed","DCAM.Home","DCAM.Warmed", 
+  "ECAM.Home","ECAM.Warmed","FCAM.Home","FCAM.Warmed","BRA.Home","BRA.Warmed","PEL.Home","PEL.Warmed",
+  "PLAT.Home","PLAT.Warmed"))
+
+windows(16.53,11.69);par(mfrow=c(3,1),mar=c(0,0,0,0),oma=c(6,9,6,5))
+#Jmax
+ylims=c(90,280)
+boxplot(Jmax~TaxTreat,data=acifits,ylim=ylims,axes=F,las=2,col=colors)
+magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+legend("topleft", legend=c(expression(Warmed~(+3.5~degree~C)),"Home"),pch=22, pt.cex=2, 
+       pt.bg=c(alpha("red",0.6),alpha("black",0.6)),
+       bty="n",cex=1.2)
+
+mtext(text=expression(J["max"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=F,cex=1,adj=0.5,line=3)
+legend("topright","a", bty="n", cex=1.5)
+
+#Vcmax
+ylims=c(60,260)
+boxplot(Vcmax~TaxTreat,data=acifits,ylim=ylims,axes=F,las=2,col=colors)
+magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+mtext(text=expression(V["cmax"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+mtext(text=expression("("*mu*mol~g^-1~s^-1*")"),side=2,outer=T,cex=1,adj=0.5,line=3)
+legend("topright","b", bty="n", cex=1.5)
+
+#Asat
+ylims=c(15,35)
+boxplot(Photo~TaxTreat,data=Asat,ylim=ylims,
+        axes=F,las=2,col=colors)
+magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+mtext(text=expression(A["sat"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+mtext(text=expression("("*mu*mol~m^-2~s^-1*")"),side=2,outer=T,cex=1,adj=0.12,line=3)
+axis(side=1,at=c(seq(1.5,34, by=2)),labels=c(levels(Asat$Taxa)),las=1,cex.axis=1.5)
+legend("topright","c", bty="n", cex=1.5)
+
+#mass based
+Asatm$Photom<-with(Asatm, Photo*SLA/10000)
+
+colors <- c(alpha("black",0.4),"red")
+acifits$TaxTreat<- as.factor(paste(acifits$Taxa,acifits$Treatment, sep="."))
+Asatm$TaxTreat<- as.factor(paste(Asatm$Taxa,Asatm$Treatment, sep="."))
+
+acifits$TaxTreat<- factor(acifits$TaxTreat, levels=c(
+  "ATER.Home","ATER.Warmed","BTER.Home","BTER.Warmed","ACAM.Home","ACAM.Warmed","BCAM.Home","BCAM.Warmed",
+  "CCAM.Home","CCAM.Warmed","BOT.Home","BOT.Warmed","LONG.Home","LONG.Warmed","SMIT.Home","SMIT.Warmed",
+  "CTER.Home","CTER.Warmed","DTER.Home","DTER.Warmed", "ETER.Home", "ETER.Warmed","DCAM.Home","DCAM.Warmed", 
+  "ECAM.Home","ECAM.Warmed","FCAM.Home","FCAM.Warmed","BRA.Home","BRA.Warmed","PEL.Home","PEL.Warmed",
+  "PLAT.Home","PLAT.Warmed"))
+Asatm$TaxTreat<- factor(Asatm$TaxTreat, levels=c(
+  "ATER.Home","ATER.Warmed","BTER.Home","BTER.Warmed","ACAM.Home","ACAM.Warmed","BCAM.Home","BCAM.Warmed",
+  "CCAM.Home","CCAM.Warmed","BOT.Home","BOT.Warmed","LONG.Home","LONG.Warmed","SMIT.Home","SMIT.Warmed",
+  "CTER.Home","CTER.Warmed","DTER.Home","DTER.Warmed", "ETER.Home", "ETER.Warmed","DCAM.Home","DCAM.Warmed", 
+  "ECAM.Home","ECAM.Warmed","FCAM.Home","FCAM.Warmed","BRA.Home","BRA.Warmed","PEL.Home","PEL.Warmed",
+  "PLAT.Home","PLAT.Warmed"))
+
+windows(16.53,11.69);par(mfrow=c(3,1),mar=c(0,0,0,0),oma=c(6,9,6,5))
+#Jmax
+ylims=c(1.5,6)
+boxplot(Jmaxm~TaxTreat,data=acifits,ylim=ylims,axes=F,las=2,col=colors)
+magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+legend("topleft", legend=c(expression(Warmed~(+3.5~degree~C)),"Home"),pch=22, pt.cex=2, 
+       pt.bg=c(alpha("red",0.6),alpha("black",0.6)),
+       bty="n",cex=1.2)
+
+mtext(text=expression(J["max"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+mtext(text=expression("("*mu*mol~g^-1~s^-1*")"),side=2,outer=F,cex=1,adj=0.5,line=3)
+legend("topright","a", bty="n", cex=1.5)
+
+#Vcmax
+ylims=c(1,5.5)
+boxplot(Vcmaxm~TaxTreat,data=acifits,ylim=ylims,axes=F,las=2,col=colors)
+magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+mtext(text=expression(V["cmax"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+mtext(text=expression("("*mu*mol~g^-1~s^-1*")"),side=2,outer=T,cex=1,adj=0.5,line=3)
+legend("topright","b", bty="n", cex=1.5)
+
+#Asat
+ylims=c(0.28,0.85)
+boxplot(Photom~TaxTreat,data=Asatm,ylim=ylims,
+        axes=F,las=2,col=colors)
+magaxis(c(2,4),labels=c(1,0),frame.plot=T,las=1,cex.axis=1.2)
+mtext(text=expression(A["sat"]),side=2,outer=F,cex=1.2,adj=0.5,line=5)
+mtext(text=expression("("*mu*mol~g^-1~s^-1*")"),side=2,outer=T,cex=1,adj=0.12,line=3)
+axis(side=1,at=c(seq(1.5,34, by=2)),labels=c(levels(Asatm$Taxa)),las=1,cex.axis=1.5)
+legend("topright","c", bty="n", cex=1.5)
--- R_cleaned/Table 3.R
+++ R_cleaned/Table 3.R
@@ -9,6 +9,20 @@ acifits$Vcmaxm<-with(acifits, Vcmax*SLA/10000)
 acifits$JtoVm<-with(acifits,Jmaxm/Vcmaxm)
 
 
+###
+# SMIT really changes things.
+# 
+# For area based rates: the only difference is that the range effect on Vcmax becomes insignificant and rates are 
+# no longer higher in wide than narrow (goes from being 14% higher to only 11 % higher than narrow).
+# 
+# For mass based rates: If SMIT is removed the Treatment * Range effects disappear for both Vcmax and Jmax as the 
+# warming effect becomes more negative. For Vcmax narrow species go from +2.4% to -3.6 % with warming. For Jmax 
+# narrow species change from being only -3.5 % to being -10 % reduced by warming.
+# 
+# I.e. SMIT did not acclimate as well as the other narrow species and it was not just because of SLA differences 
+# as it still makes a big difference whether or not including SMIT in mass based rates.
+###
+
 ## area based results with SMIT
 fm.Asat <- lme(Photo~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=Asat)
 plot(fm.Asat,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
@@ -16,9 +30,9 @@ plot(fm.Asat,Photo~fitted(.)|Species,abline=c(0,1))            #predicted vs. fi
 plot(fm.Asat,Photo~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
 qqnorm(fm.Asat, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.Asat$residuals[,1])
-anova(fm.Asat)    #no effects, still no effects if SMIT is removed
+anova(fm.Asat)    
 effect("Treatment",fm.Asat)
-(26.72886-28.35335)/28.35335 #-5.7% with warming #-6 % if SMIT is removed
+(26.72886-28.35335)/28.35335 #-5.7% with warming
 
 fm.vcmax <- lme(log(Vcmax)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifits)
 plot(fm.vcmax,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
@@ -27,9 +41,12 @@ plot(fm.vcmax,log(Vcmax)~fitted(.),abline=c(0,1))                    #overall pr
 qqnorm(fm.vcmax, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.vcmax$residuals[,1])
 anova(fm.vcmax)    
+plot(effect("Range",fm.vcmax))      #- Vcmax higher in wide than narrow
+(exp(4.911595)-exp(4.783875))/(exp(4.783875))    # 13.6% higher in wide
+
 plot(effect("Treatment:Location",fm.vcmax))      #- warming reduced Vcmax in the south but not in the north. No range interactions
-(exp(4.433354)-exp(4.612889))/(exp(4.612889))    # 16.4 reduction in Vcmax in S # -18.8 % if SMIT is removed
-(exp(5.164627)-exp(5.195303))/(exp(5.195303)) #  3% reduction in Vcmax in N 
+(exp(4.433354)-exp(4.612889))/(exp(4.612889))    # -16.4 % in Vcmax in S
+(exp(5.164627)-exp(5.195303))/(exp(5.195303))    #  -3%  in Vcmax in N 
 
 
 fm.jmax <- lme(log(Jmax)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifits)
@@ -40,8 +57,8 @@ qqnorm(fm.jmax, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to asses
 hist(fm.jmax$residuals[,1])
 anova(fm.jmax)    
 plot(effect("Treatment:Location",fm.jmax))
-(exp(4.914717)-exp(5.153855))/(exp(5.153855))    # 21.3% reduction in Jmax in S #-23.2 % if SMIT is removed
-(exp(5.006654)-exp(5.108184))/(exp(5.108184))    #  9.7% reduction in Jmax in N 
+(exp(4.914717)-exp(5.153855))/(exp(5.153855))    # -21.3% in Jmax in S
+(exp(5.006654)-exp(5.108184))/(exp(5.108184))    #  -9.7%  in Jmax in N 
 
 
 fm.jtov <- lme(log(JtoV)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifits)
@@ -52,9 +69,9 @@ qqnorm(fm.jtov, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to asses
 hist(fm.jtov$residuals[,1])
 anova(fm.jtov)
 effect("Treatment",fm.jtov)
-(exp(0.1468196)-exp(0.2121764))/(exp(0.2121764))      # 6.3% reduction in Jmax/Vcmax with warming #-6.1 % if SMIT removed
+(exp(0.1468196)-exp(0.2121764))/(exp(0.2121764))      # -6.3% in Jmax/Vcmax with warming
 effect("Location",fm.jtov)
-(exp(-0.1228216)-exp(0.5110372))/(exp(0.5110372))      # 46.9% lower Jmax/Vcmax in N compared to S #-46.3% if SMIT removed
+(exp(-0.1228216)-exp(0.5110372))/(exp(0.5110372))      # 46.9% lower Jmax/Vcmax in N compared to S
 
 
 # --- Area based results w/o SMIT  ------------------------------------------------------------
@@ -70,9 +87,9 @@ plot(fm.Asat,Photo~fitted(.)|Species,abline=c(0,1))            #predicted vs. fi
 plot(fm.Asat,Photo~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
 qqnorm(fm.Asat, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.Asat$residuals[,1])
-anova(fm.Asat)    #no effects, still no effects if SMIT is removed
+anova(fm.Asat)   
 effect("Treatment",fm.Asat)
-(26.72886-28.35335)/28.35335 #-5.7% with warming #-6 % if SMIT is removed
+(26.82011-28.43675)/28.43675  #-5.7% with warming
 
 fm.vcmax <- lme(log(Vcmax)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifitss)
 plot(fm.vcmax,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
@@ -80,10 +97,13 @@ plot(fm.vcmax,log(Vcmax)~fitted(.)|Species,abline=c(0,1))            #predicted 
 plot(fm.vcmax,log(Vcmax)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
 qqnorm(fm.vcmax, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.vcmax$residuals[,1])
-anova(fm.vcmax)    
+anova(fm.vcmax)
+plot(effect("Range",fm.vcmax))      #- Vcmax higher in wide than narrow
+(exp(4.931121)-exp(4.827046))/(exp(4.827046))    # 10.9% higher in wide
+
 plot(effect("Treatment:Location",fm.vcmax))      #- warming reduced Vcmax in the south but not in the north. No range interactions
-(exp(4.433354)-exp(4.612889))/(exp(4.612889))    # 16.4 reduction in Vcmax in S # -18.8 % if SMIT is removed
-(exp(5.164627)-exp(5.195303))/(exp(5.195303)) #  3% reduction in Vcmax in N 
+(exp(4.440326)-exp(4.649033))/(exp(4.649033))    # -18.8 % in S
+(exp(5.167493)-exp(5.199305))/(exp(5.199305))    #  -3%  in Vcmax in N 
 
 
 fm.jmax <- lme(log(Jmax)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifitss)
@@ -94,8 +114,8 @@ qqnorm(fm.jmax, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to asses
 hist(fm.jmax$residuals[,1])
 anova(fm.jmax)    
 plot(effect("Treatment:Location",fm.jmax))
-(exp(4.914717)-exp(5.153855))/(exp(5.153855))    # 21.3% reduction in Jmax in S #-23.2 % if SMIT is removed
-(exp(5.006654)-exp(5.108184))/(exp(5.108184))    #  9.7% reduction in Jmax in N 
+(exp(4.912706)-exp(5.176771))/(exp(5.176771))    # -23.2%  in Jmax in S 
+(exp(5.011273)-exp(5.113089))/(exp(5.113089))    #  -9.7%  in Jmax in N 
 
 
 fm.jtov <- lme(log(JtoV)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifitss)
@@ -106,23 +126,24 @@ qqnorm(fm.jtov, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to asses
 hist(fm.jtov$residuals[,1])
 anova(fm.jtov)
 effect("Treatment",fm.jtov)
-(exp(0.1468196)-exp(0.2121764))/(exp(0.2121764))      # 6.3% reduction in Jmax/Vcmax with warming #-6.1 % if SMIT removed
+(exp(0.1235163)-exp(0.1869347))/(exp(0.1869347))      # -6.1%  in Jmax/Vcmax with warming
 effect("Location",fm.jtov)
-(exp(-0.1228216)-exp(0.5110372))/(exp(0.5110372))      # 46.9% lower Jmax/Vcmax in N compared to S #-46.3% if SMIT removed
+(exp(-0.1216975)-exp(0.4997415))/(exp(0.4997415))      # -46.3% in N
 
-
+##----------------------------------
+##----------------------------------
 ##----------------------------------
 #    mass based rates
 
 fm.Asatm <- lme(sqrt(Photom)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=Asatm)
 plot(fm.Asatm,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
-plot(fm.Asatm,Photom~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
-plot(fm.Asatm,Photom~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
+plot(fm.Asatm,sqrt(Photom)~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
+plot(fm.Asatm,sqrt(Photom)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
 qqnorm(fm.Asatm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.Asatm$residuals[,1])
 anova(fm.Asatm)    #no effects, still no effects if SMIT is removed
 plot(effect("Treatment:Location",fm.Asatm))
-((22.64264)^2-(21.61941)^2)/((21.61941)^2)    # 9.7% increase in Asat in S # 
+((22.64264)^2-(21.61941)^2)/((21.61941)^2)    # +9.7% increase in Asat in S # 
 ((22.27189)^2-(23.38046)^2)/((23.38046)^2) #  -9.3% decrease in Asat in N
 
 fm.vcmaxm <- lme(sqrt(Vcmaxm)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifits)
@@ -133,20 +154,20 @@ qqnorm(fm.vcmaxm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to ass
 hist(fm.vcmaxm$residuals[,1])
 anova(fm.vcmaxm)    
 plot(effect("Treatment:Range",fm.vcmaxm))      #- warming reduced Vcmax in the wide but not in the narrow. No location interactions
-((1.554706)^2-(1.536480)^2)/((1.536480)^2)    # 2% reduction in Vcmax in narrow # 
-((1.560650)^2-(1.639508)^2)/((1.639508)^2) #  2.3% increase in Vcmax in N
+((1.554706^2)-(1.536480^2))/((1.536480^2))    # +2.4%  in Vcmax in narrow # 
+((1.560650^2)-(1.639508^2))/((1.639508^2)) #  -9.4%  in Vcmax in wide
 
 #- fit and interpret Jmax
 fm.jmaxm <- lme(sqrt(Jmaxm)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifits)
 plot(fm.jmaxm,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
-plot(fm.jmaxm,log(Jmaxm)~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
-plot(fm.jmaxm,log(Jmaxm)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
+plot(fm.jmaxm,sqrt(Jmaxm)~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
+plot(fm.jmaxm,sqrt(Jmaxm)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
 qqnorm(fm.jmaxm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.jmaxm$residuals[,1])
 anova(fm.jmaxm)    
 plot(effect("Treatment:Range",fm.jmaxm))      #- warming reduced Jmax in the wide but not in the narrow No location interactions
-((1.660084)^2-(1.689630)^2)/((1.689630)^2)    # 3.5% reduction in Jmax in narrow #
-((1.653057)^2-(1.786284)^2)/((1.786284)^2) #  -14.4% decrease in Jmax in wide
+((1.660084)^2-(1.689630)^2)/((1.689630)^2)    # -3.5%  in Jmax in narrow #
+((1.653057)^2-(1.786284)^2)/((1.786284)^2)    #  -14.4%  in Jmax in wide
 
 
 #- fit and interpret Jmax/Vcmax
@@ -157,6 +178,10 @@ plot(fm.jtovm,log(JtoVm)~fitted(.),abline=c(0,1))                    #overall pr
 qqnorm(fm.jtovm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.jtovm$residuals[,1])
 anova(fm.jtovm)
+effect("Treatment",fm.jtovm)
+(exp(0.1468196)-exp(0.2121764))/(exp(0.2121764))      # -6.3%  in Jmax/Vcmax with warming
+effect("Location",fm.jtovm)
+(exp(-0.1228216)-exp(0.5110372))/(exp(0.5110372))      # -46.9% in N
 
 
 #-------------------------------
@@ -168,14 +193,14 @@ acifitss<- subset(acifits, Taxa != "SMIT")
 
 fm.Asatm <- lme(sqrt(Photom)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=Asatms)
 plot(fm.Asatm,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
-plot(fm.Asatm,Photom~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
-plot(fm.Asatm,Photom~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
+plot(fm.Asatm,sqrt(Photom)~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
+plot(fm.Asatm,sqrt(Photom)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
 qqnorm(fm.Asatm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.Asatm$residuals[,1])
 anova(fm.Asatm)    #no effects, still no effects if SMIT is removed
 plot(effect("Treatment:Location",fm.Asatm))
-((22.64264)^2-(21.61941)^2)/((21.61941)^2)    # 9.7% increase in Asat in S # 
-((22.27189)^2-(23.38046)^2)/((23.38046)^2) #  -9.3% decrease in Asat in N
+((22.35356)^2-(21.55693)^2)/((21.55693)^2)    # +7.5% increase in Asat in S # 
+((22.28974)^2-(23.43371)^2)/((23.43371)^2) #  -9.5% decrease in Asat in N
 
 fm.vcmaxm <- lme(sqrt(Vcmaxm)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifitss)
 plot(fm.vcmaxm,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
@@ -185,20 +210,20 @@ qqnorm(fm.vcmaxm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to ass
 hist(fm.vcmaxm$residuals[,1])
 anova(fm.vcmaxm)    
 plot(effect("Treatment:Range",fm.vcmaxm))      #- warming reduced Vcmax in the wide but not in the narrow. No location interactions
-((1.554706)^2-(1.536480)^2)/((1.536480)^2)    # 2% reduction in Vcmax in narrow # 
-((1.560650)^2-(1.639508)^2)/((1.639508)^2) #  2.3% increase in Vcmax in N
+((1.548087)^2-(1.577019)^2)/((1.577019)^2)    # -3.6% in Vcmax in narrow # 
+((1.577773)^2-(1.658058)^2)/((1.658058)^2) #  -9.4% in Vcmax in N
 
 #- fit and interpret Jmax
 fm.jmaxm <- lme(sqrt(Jmaxm)~Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),data=acifitss)
 plot(fm.jmaxm,resid(.,type="p")~fitted(.) | Treatment,abline=0)   #resid vs. fitted for each treatment. Is variance approximately constant?
-plot(fm.jmaxm,log(Jmaxm)~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
-plot(fm.jmaxm,log(Jmaxm)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
+plot(fm.jmaxm,sqrt(Jmaxm)~fitted(.)|Species,abline=c(0,1))            #predicted vs. fitted for each species
+plot(fm.jmaxm,sqrt(Jmaxm)~fitted(.),abline=c(0,1))                    #overall predicted vs. fitted
 qqnorm(fm.jmaxm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.jmaxm$residuals[,1])
 anova(fm.jmaxm)    
 plot(effect("Treatment:Range",fm.jmaxm))      #- warming reduced Jmax in the wide but not in the narrow No location interactions
-((1.660084)^2-(1.689630)^2)/((1.689630)^2)    # 3.5% reduction in Jmax in narrow #
-((1.653057)^2-(1.786284)^2)/((1.786284)^2) #  -14.4% decrease in Jmax in wide
+((1.621423)^2-(1.710628)^2)/((1.710628)^2)    # -10.2% reduction in Jmax in narrow #
+((1.655986)^2-(1.790320)^2)/((1.790320)^2) #  -14.4% decrease in Jmax in wide
 
 
 #- fit and interpret Jmax/Vcmax
@@ -209,4 +234,7 @@ plot(fm.jtovm,log(JtoVm)~fitted(.),abline=c(0,1))                    #overall pr
 qqnorm(fm.jtovm, ~ resid(., type = "p"), abline = c(0, 1))       #qqplot to assess normality of residuals
 hist(fm.jtovm$residuals[,1])
 anova(fm.jtovm)
-
+effect("Treatment",fm.jtovm)
+(exp(0.1235163)-exp(0.1869347))/(exp(0.1869347))      # -6.1%  in Jmax/Vcmax with warming
+effect("Location",fm.jtovm)
+(exp(-0.1216975)-exp(0.4997415))/(exp(0.4997415))      # -46.2% in N
