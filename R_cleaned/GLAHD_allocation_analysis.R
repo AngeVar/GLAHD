2f9136a6bf8f703811b89815d85c4a2e4841abf7
Angelica Varhammar <30034792@westernsydney.edu.au>
2017-06-06 16:05:09 +1000

    changing data cut off from a date to the actual mass, "remove anything that is higher than 14 grams" (more than 2 g DW per L pot volume)

--- R_cleaned/GLAHD_allocation_analysis.R
+++ R_cleaned/GLAHD_allocation_analysis.R
@@ -2,12 +2,12 @@
 
 #use data2: harvest dataset
 #as.Date("2014-11-07") + 30 - use only data from the first 30 days before plants got pot bound
-
+summaryBy(Pot~Treatment+Taxa, data=subset(data2, Totmass <=14), FUN =length)
 
 #Leaf mass over Total mass = LMF
 
 fm1LM <- lme(log(Leafmass)~log(Totmass)*Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),
-             data=subset(data2,Date < as.Date("2014-12-07")), method="ML")
+             data=subset(data2,Totmass <=14), method="ML")
 plot(fm1LM,resid(.,type="p")~fitted(.) | Treatment,abline=0)     #resid vs. fitted for each treatment. Is variance approximately constant?
 plot(fm1LM,log(Leafmass)~fitted(.)|Species,abline=c(0,1))               #predicted vs. fitted for each species
 plot(fm1LM,log(Leafmass)~fitted(.),abline=c(0,1))                       #overall predicted vs. fitted
@@ -17,42 +17,42 @@ anova(fm1LM)
 
 fm1.1LM <- lme(log(Leafmass)~log(Totmass)+Treatment+Location+Treatment:Location,
              random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),
-             data=subset(data2,Date < as.Date("2014-12-07")))#, method="ML")
+             data=subset(data2,Totmass <=14))#, method="ML")
 anova(fm1.1LM,fm1LM)
 plot(effect("Treatment:Location",fm1.1LM), multiline=T)
-(exp(1.062823)-exp(1.131168))/exp(1.131168) 
-(exp(0.9972767)-exp(0.9940873))/exp(0.9940873)
+(exp(1.071134)-exp(1.056171))/exp(1.056171) #+1.5% in S
+(exp(1.145197)-exp(1.206949))/exp(1.206949) #-6% in N
 # The slopes (i.e. allocation) did not change.
-# But there is a significant treatment by location interaction (0.0258) where warming reduced LMF in N (-6.6%) but 
-# not S (+0.3%)
+# But there is a significant treatment by location interaction (0.0258) where warming reduced LMF in N (-6%) but 
+# not S (+1.5%)
 
 
 windows(10,7)
 par(mfrow=c(1,2),mar=c(3,0,1.5,0),oma=c(6,7,6,7),cex.axis=1.2)
-plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Totmass <=14), 
        axes=F, ylim= c(-1.2,2.5), xlim=c(-1,3))
-lm1<- lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Date < as.Date("2014-12-07")))
+lm1<- lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Totmass <=14))
 abline(lm1, col="red")
-abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black")
+abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Totmass <=14)), col="black")
 
 magaxis(c(1,2),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
 mtext(text="Temperate", side=3, line=0.5, cex=1.2)
 
-plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Totmass<=14), 
        ylim= c(-1.2,2.5), xlim=c(-1,3), legend =F,axes=F)
 magaxis(c(1,4),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
-abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Date < as.Date("2014-12-07"))), col="red")
-abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black")
+abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Totmass<=14)), col="red")
+abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Totmass<=14)), col="black")
 mtext(text="Tropical", side=3, line=0.5, cex=1.2)
 mtext(text="ln(Leaf mass)",side=2,outer=T,cex=1.2,adj=0.5,line=3)
-mtext(text="ln(Total biomass)",side=1,outer=T,cex=1.2,adj=0.5,line=1)
+mtext(text="ln(Total mass)",side=1,outer=T,cex=1.2,adj=0.5,line=1)
 
 
 #######################################
 #Leaf area - over Leaf mass = SLA
 #            
 fm2LM <- lme(log(Leafarea)~log(Leafmass)*Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),
-             data=subset(data2,Date < as.Date("2014-12-07")), method="ML")
+             data=subset(data2,Totmass <=14), method="ML")
 plot(fm2LM,resid(.,type="p")~fitted(.) | Treatment,abline=0)     #resid vs. fitted for each treatment. Is variance approximately constant?
 plot(fm2LM,log(Leafarea)~fitted(.)|Species,abline=c(0,1))               #predicted vs. fitted for each species
 plot(fm2LM,log(Leafarea)~fitted(.),abline=c(0,1))                       #overall predicted vs. fitted
@@ -63,12 +63,12 @@ anova(fm2LM)
 fm2.1LM <- lme(log(Leafarea)~log(Leafmass)+Treatment+Location+
                Treatment:Location,
              random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),
-             data=subset(data2,Date < as.Date("2014-12-07")))#, method="ML")
+             data=subset(data2,Totmass <=14))#, method="ML")
 anova(fm2.1LM)
 anova(fm2LM,fm2.1LM)
 plot(effect("Treatment:Location",fm2.1LM), multiline=T)
-(exp(6.294555)-exp(6.414016))/exp(6.414016) #-11.3%
-(exp(6.349975)-exp(6.209696))/exp(6.209696) #+15%
+(exp(6.472118)-exp(6.332865))/exp(6.332865) #+14.9% in S
+(exp(6.370302)-exp(6.522173))/exp(6.522173) #-14.1% in N
 
 # The slopes (i.e. allocation) did not change.
 # But there is a significant treatment by location interaction (0.0014) where warming reduced SLA in N (-11.3%) 
@@ -76,20 +76,20 @@ plot(effect("Treatment:Location",fm2.1LM), multiline=T)
 
 windows(10,7)
 par(mfrow=c(1,2),mar=c(3,0,1.5,0),oma=c(6,7,6,7),cex.axis=1.2)
-plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "S"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "S"&Totmass<=14), 
        axes=F, ylim= c(3.5,8), xlim=c(-1.5,2.5))
-lm1<- lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Date < as.Date("2014-12-07")))
+lm1<- lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Totmass<=14))
 abline(lm1, col="red")
-abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black")
+abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Home"&Totmass<=14)), col="black")
 
 magaxis(c(1,2),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
 mtext(text="Temperate", side=3, line=0.5, cex=1.2)
 
-plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "N"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "N"&Totmass<=14), 
        ylim= c(3.5,8), xlim=c(-1.5,2.5), legend =F,axes=F)
 magaxis(c(1,4),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
-abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Date < as.Date("2014-12-07"))), col="red")
-abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black")
+abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Totmass<=14)), col="red")
+abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Home"&Totmass<=14)), col="black")
 mtext(text="Tropical", side=3, line=0.5, cex=1.2)
 mtext(text="ln(Leaf mass)",side=2,outer=T,cex=1.2,adj=0.5,line=3)
 mtext(text="ln(Total Leaf mass)",side=1,outer=T,cex=1.2,adj=0.5,line=1)
@@ -99,7 +99,7 @@ mtext(text="ln(Total Leaf mass)",side=1,outer=T,cex=1.2,adj=0.5,line=1)
 #Leaf area - over Total mass = LAR
 #            
 fm3LM <- lme(log(Leafarea)~log(Totmass)*Treatment*Location*Range,random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),
-             data=subset(data2,Date < as.Date("2014-12-07")), method="ML")
+             data=subset(data2,Totmass<=14), method="ML")
 plot(fm3LM,resid(.,type="p")~fitted(.) | Treatment,abline=0)     #resid vs. fitted for each treatment. Is variance approximately constant?
 plot(fm3LM,log(Leafarea)~fitted(.)|Species,abline=c(0,1))               #predicted vs. fitted for each species
 plot(fm3LM,log(Leafarea)~fitted(.),abline=c(0,1))                       #overall predicted vs. fitted
@@ -110,12 +110,12 @@ anova(fm3LM)
 fm3.1LM <- lme(log(Leafarea)~log(Totmass)+Treatment+Location+
                      Treatment:Location,
                    random=list(~1|Sp_RS_EN,~1|Prov_Sp_EN),
-               data=subset(data2,Date < as.Date("2014-12-07")))#, method="ML")
+               data=subset(data2,Totmass<=14))#, method="ML")
 anova(fm3.1LM)
 anova(fm3LM,fm3.1LM)
 plot(effect("Treatment:Location",fm3.1LM), multiline=T)
-(exp(6.315378)-exp(6.504422))/exp(6.504422) #-17.2%
-(exp(6.308370)-exp(6.164833))/exp(6.164833) #+15.4%
+(exp(6.438823)-exp(6.282291))/exp(6.282291) #-+16.9% in S
+(exp(6.406582)-exp(6.615842))/exp(6.615842) #-18.8% in N
 
 # The slopes (i.e. allocation) did not change.
 # But there is a significant treatment by location interaction (0.0001) where warming reduced SLA in N (-17.2%) 
@@ -123,24 +123,24 @@ plot(effect("Treatment:Location",fm3.1LM), multiline=T)
 
 windows(10,7)
 par(mfrow=c(1,2),mar=c(3,0,1.5,0),oma=c(6,7,6,7),cex.axis=1.2)
-plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Totmass<=14), 
        axes=F, ylim= c(3.5,8), xlim=c(-0.5,3))
-lm1<- lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Date < as.Date("2014-12-07")))
+lm1<- lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Totmass<=14))
 abline(lm1, col="red")
-abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black",
+abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Totmass<=14)), col="black",
        xlim=c(-1,3))
 
 magaxis(c(1,2),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
 mtext(text="Temperate", side=3, line=0.5, cex=1.2)
 
-plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Totmass<=14), 
        ylim= c(3.5,8), xlim=c(-0.5,3), legend =F,axes=F)
 magaxis(c(1,4),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
-abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Date < as.Date("2014-12-07"))), col="red")
-abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black")
+abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Totmass<=14)), col="red")
+abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Totmass<=14)), col="black")
 mtext(text="Tropical", side=3, line=0.5, cex=1.2)
 mtext(text="ln(Leaf area)",side=2,outer=T,cex=1.2,adj=0.5,line=3)
-mtext(text="ln(Total biomass)",side=1,outer=T,cex=1.2,adj=0.5,line=1)
+mtext(text="ln(Total mass)",side=1,outer=T,cex=1.2,adj=0.5,line=1)
 
 ###
 #plot them all together
@@ -149,64 +149,64 @@ windows(10,10)
 par(mfrow=c(3,2),mar=c(3,0,1.5,0),oma=c(6,10,6,7),cex.axis=1.2)
 
 #LAR
-plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Totmass<=14), 
        axes=F, ylim= c(3.5,8.5), xlim=c(-0.5,3),pch=19,cex=2, legend =F)
 legend(-0.5,8, legend=c(expression(Warmed~(+3.5~degree~C)),"Home"),col=c("red","black"),pch=19,bty="n", cex=1.2, pt.cex =2)
-lm1<- lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Date < as.Date("2014-12-07")))
+lm1<- lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Totmass<=14))
 abline(lm1, col="red",lwd=2)
-abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black",
+abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Totmass<=14)), col="black",
        xlim=c(-1,3),lwd=2)
 magaxis(c(1,2),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
 mtext(text="Temperate", side=3, line=0.5, cex=1.2)
 legend("topright","a", bty="n", cex=1.5)
 
-plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Totmass<=14), 
        ylim= c(3.5,8.5), xlim=c(-0.5,3), legend =F,axes=F,pch=19,cex=2)
 magaxis(c(1,4),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
-abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Date < as.Date("2014-12-07"))), col="red",lwd=2)
-abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black",lwd=2)
+abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Totmass<=14)), col="red",lwd=2)
+abline(lm(log(Leafarea)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Totmass<=14)), col="black",lwd=2)
 mtext(text="Tropical", side=3, line=0.5, cex=1.2)
 mtext(text="ln(Leaf area (cm2))",side=2,outer=T,cex=1,adj=0.92,line=2.5)
-mtext(text="ln(Total biomass (g))",side=1,outer=T,cex=1,line=-41.5)
+mtext(text="ln(Total mass (g))",side=1,outer=T,cex=1,line=-41.5)
 mtext(text="LAR",side=2,outer=T,cex=1.5,adj=0.88,line=5)
 legend("topright","b", bty="n", cex=1.5)
 
 #SLA
 
-plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "S"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "S"&Totmass<=14), 
        axes=F, ylim= c(3.5,8.5), xlim=c(-1.5,2.5), legend=F,pch=19,cex=2)
-lm1<- lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Date < as.Date("2014-12-07")))
+lm1<- lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Totmass<=14))
 abline(lm1, col="red",lwd=2)
-abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black",lwd=2)
+abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "S"& Treatment == "Home"&Totmass<=14)), col="black",lwd=2)
 magaxis(c(1,2),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
 legend("topright","c", bty="n", cex=1.5)
 
-plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "N"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafarea)~log(Leafmass)|Treatment, data=subset(data2,Location == "N"&Totmass<=14), 
        ylim= c(3.5,8.5), xlim=c(-1.5,2.5), legend =F,axes=F,pch=19,cex=2)
 magaxis(c(1,4),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
-abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Date < as.Date("2014-12-07"))), col="red",lwd=2)
-abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black",lwd=2)
+abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Totmass<=14)), col="red",lwd=2)
+abline(lm(log(Leafarea)~log(Leafmass), data=subset(data2,Location == "N"& Treatment == "Home"&Totmass<=14)), col="black",lwd=2)
 mtext(text="ln(Leaf area (cm2))",side=2,outer=T,cex=1,adj=0.5,line=2.5)
 mtext(text="ln(Total Leaf mass (g))",side=1,outer=T,cex=1,adj=0.5,line=-21)
 mtext(text="SLA",side=2,outer=T,cex=1.5,adj=0.5,line=5)
 legend("topright","d", bty="n", cex=1.5)
 
 #LMF
-plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "S"&Totmass<=14), 
        axes=F, ylim= c(-1.2,3), xlim=c(-1,3), legend=F,pch=19,cex=2)
-lm1<- lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Date < as.Date("2014-12-07")))
+lm1<- lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Warmed"&Totmass<=14))
 abline(lm1, col="red",lwd=2)
-abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black",lwd=2)
+abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "S"& Treatment == "Home"&Totmass<=14)), col="black",lwd=2)
 magaxis(c(1,2),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
 legend("topright","e", bty="n", cex=1.5)
 
-plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Date < as.Date("2014-12-07")), 
+plotBy(log(Leafmass)~log(Totmass)|Treatment, data=subset(data2,Location == "N"&Totmass<=14), 
        ylim= c(-1.2,3), xlim=c(-1,3), legend =F,axes=F,pch=19,cex=2)
 magaxis(c(1,4),labels=c(1,1),frame.plot=T,las=1,cex.axis=1.2)
-abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Date < as.Date("2014-12-07"))), col="red",lwd=2)
-abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Date < as.Date("2014-12-07"))), col="black",lwd=2)
+abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Warmed"&Totmass<=14)), col="red",lwd=2)
+abline(lm(log(Leafmass)~log(Totmass), data=subset(data2,Location == "N"& Treatment == "Home"&Totmass<=14)), col="black",lwd=2)
 mtext(text="ln(Leaf mass (g))",side=2,outer=T,cex=1,adj=0.1,line=2.5)
-mtext(text="ln(Total biomass (g))",side=1,outer=T,cex=1,adj=0.5,line=-1)
+mtext(text="ln(Total mass (g))",side=1,outer=T,cex=1,adj=0.5,line=-1)
 mtext(text="LMF",side=2,outer=T,cex=1.5,adj=0.15,line=5)
 legend("topright","f", bty="n", cex=1.5)
 
